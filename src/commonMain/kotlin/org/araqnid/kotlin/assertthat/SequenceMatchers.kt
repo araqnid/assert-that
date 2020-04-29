99d7b3a415675815316f9e93b923ca41cef682a2
Steve Haslam <araqnid@googlemail.com>
2020-04-30 00:44:58 +0100

    Support matching a sequence with a coroutine

--- src/commonMain/kotlin/org/araqnid/kotlin/assertthat/SequenceMatchers.kt
+++ src/commonMain/kotlin/org/araqnid/kotlin/assertthat/SequenceMatchers.kt
@@ -1,5 +1,14 @@
 package org.araqnid.kotlin.assertthat
 
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.RestrictsSuspension
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.startCoroutine
+import kotlin.coroutines.suspendCoroutine
+
 fun <T> producesSequence(vararg matchers: Matcher<T>) = object : Matcher<Sequence<T>> {
     override fun match(actual: Sequence<T>): AssertionResult {
         val expectedIterator = matchers.iterator()
@@ -27,3 +36,126 @@ fun <T> producesSequence(vararg matchers: Matcher<T>) = object : Matcher<Sequenc
 }
 
 val producesEmptySequence = producesSequence<Any?>().describedBy { "empty sequence" }
+
+@RestrictsSuspension
+interface SequenceMatcherScope<out T> {
+    suspend fun take(): T
+
+    val index: Int
+
+    suspend fun assertNext(matcher: Matcher<T>) {
+        val value = try {
+            take()
+        } catch (e: InputExhaustedException) {
+            throw SequenceMismatchException(AssertionResult.Mismatch("item #$index not available (expected ${describe(matcher)})"))
+        }
+
+        val match = matcher.match(value)
+        if (match is AssertionResult.Mismatch)
+            throw SequenceMismatchException(match.mapMessage { "item #$index $it" })
+    }
+
+    suspend fun assertRemaining(matcher: Matcher<List<T>>) {
+        val values = mutableListOf<T>()
+        try {
+            while (true) {
+                values += take()
+            }
+        } catch (e: InputExhaustedException) {
+            // ignore
+        }
+
+        val match = matcher.match(values)
+        if (match is AssertionResult.Mismatch)
+            throw SequenceMismatchException(match.mapMessage { "remaining items $it" })
+    }
+}
+
+suspend fun <T : Any> SequenceMatcherScope<T>.takeOrNull(): T? {
+    return try {
+        take()
+    } catch (e: InputExhaustedException) {
+        null
+    }
+}
+
+class SequenceMismatchException(val mismatch: AssertionResult.Mismatch) : Exception()
+
+class InputExhaustedException : IllegalStateException("Input exhausted")
+
+fun <T> producesSequence(description: String, body: suspend SequenceMatcherScope<T>.() -> Unit): Matcher<Sequence<T>> {
+    return object : Matcher<Sequence<T>> {
+        override fun match(actual: Sequence<T>): AssertionResult {
+            val scope = object : SequenceMatcherScope<T>, Continuation<Unit> {
+                init {
+                    body.startCoroutine(this, this)
+                }
+
+                override var index = 0
+                var isFinished = false
+                var failureException: Throwable? = null
+                var continuation: Continuation<T>? = null
+
+                override suspend fun take(): T {
+                    return suspendCoroutine { cont ->
+                        continuation = cont
+                    }
+                }
+
+                override val context: CoroutineContext
+                    get() = EmptyCoroutineContext
+
+                override fun resumeWith(result: Result<Unit>) {
+                    isFinished = true
+                    failureException = result.exceptionOrNull()
+                }
+
+                private fun pullContinuation(): Continuation<T> {
+                    val pulled = continuation
+                    continuation = null
+                    check(pulled != null) { "matcher should be waiting for input" }
+                    return pulled
+                }
+
+                fun push(value: T): AssertionResult.Mismatch? {
+                    check(!isFinished) { "matcher has already terminated" }
+                    pullContinuation().resume(value)
+                    ++index
+                    return when {
+                        failureException is SequenceMismatchException -> (failureException as SequenceMismatchException).mismatch
+                        failureException != null -> throw failureException!!
+                        else -> null
+                    }
+                }
+
+                fun finish(): AssertionResult {
+                    if (isFinished) return AssertionResult.Match
+
+                    while (!isFinished) {
+                        pullContinuation().resumeWithException(InputExhaustedException())
+                    }
+
+                    return when {
+                        failureException is SequenceMismatchException -> (failureException as SequenceMismatchException).mismatch
+                        failureException != null -> throw failureException!!
+                        else -> AssertionResult.Match
+                    }
+                }
+            }
+
+            val iterator = actual.iterator()
+            while (iterator.hasNext() && !scope.isFinished) {
+                val result = scope.push(iterator.next())
+                if (result != null) return result
+            }
+            if (iterator.hasNext()) {
+                val residual = iterator.asSequence().toList()
+                return AssertionResult.Mismatch("produced additional items: ${residual.map { describe(it) }}")
+            }
+
+            return scope.finish()
+        }
+
+        override val description: String = description
+    }
+}
--- src/commonTest/kotlin/org/araqnid/kotlin/assertthat/SequenceMatchersTest.kt
+++ src/commonTest/kotlin/org/araqnid/kotlin/assertthat/SequenceMatchersTest.kt
@@ -21,4 +21,29 @@ class SequenceMatchersTest {
         assertEquals(AssertionResult.Mismatch("produced more than 1 items: [\"blue\"]"), matcher.match(sequenceOf("red", "blue")))
         assertEquals("sequence where item #0 is is equal to \"red\"", matcher.description)
     }
+
+    @Test
+    fun match_sequence_with_coroutine_item_by_item() {
+        val matcher = producesSequence<String>("sequence of \"red\" matched by coroutine") {
+            assertNext(equalTo("red"))
+        }
+
+        assertEquals(AssertionResult.Match, matcher.match(sequenceOf("red")))
+        assertEquals(AssertionResult.Mismatch("item #0 not available (expected is equal to \"red\")"), matcher.match(emptySequence()))
+        assertEquals(AssertionResult.Mismatch("item #0 was \"blue\""), matcher.match(sequenceOf("blue")))
+        assertEquals(AssertionResult.Mismatch("produced additional items: [\"blue\"]"), matcher.match(sequenceOf("red", "blue")))
+        assertEquals("sequence of \"red\" matched by coroutine", matcher.description)
+    }
+
+    @Test
+    fun match_sequence_with_coroutine_matching_remaining_items() {
+        val matcher = producesSequence<String>("sequence of \"red\" followed by 2 items") {
+            assertNext(equalTo("red"))
+            assertRemaining(hasSize(2))
+        }
+
+        assertEquals(AssertionResult.Match, matcher.match(sequenceOf("red", "x", "y")))
+        assertEquals(AssertionResult.Mismatch("remaining items had size that was 1"), matcher.match(sequenceOf("red", "blue")))
+        assertEquals("sequence of \"red\" followed by 2 items", matcher.description)
+    }
 }
